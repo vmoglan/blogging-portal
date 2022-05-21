48d747f04f9786d7faa5404c2446beb4c3b298aa
Vlad Moglan <vlad.moglan@gmail.com>
2022-05-21 13:27:28 +0200

    Added a placeholder for the password.

--- app/components/login-form.js
+++ app/components/login-form.js
@@ -1,3 +1,8 @@
 import Component from '@glimmer/component';
 
-export default class LoginFormComponent extends Component {}
+export default class LoginFormComponent extends Component {
+    constructor() {
+        super(...arguments);
+        console.log(this.args.title);
+    }
+}
