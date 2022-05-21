f3c8920b352ef03123871ec297f67c48891b8f5a
Vlad Moglan <vlad.moglan@gmail.com>
2022-05-21 16:51:50 +0200

    Added some user service mocking.

--- app/components/login-form.js
+++ app/components/login-form.js
@@ -1,8 +1,18 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
-export default class LoginFormComponent extends Component {
-    constructor() {
-        super(...arguments);
-        console.log(this.args.title);
+export default class LoginFormComponent extends Component { 
+    @service user;
+
+    @tracked username;
+    @tracked password;
+
+    @action login(/* event */) {
+        this.user.login({ 
+            username: this.username, 
+            password: this.password 
+        });
     }
 }
--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -1,3 +1,8 @@
 import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
 
-export default class NavBar extends Component { }
+export default class NavBar extends Component { 
+    @service user;
+
+    get showUserMenu() { return this.user.isAuthenticated; }
+}
